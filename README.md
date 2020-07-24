ec157e0d63789c73739c33a2ab68b0eda4b65012
jehanzebahsan <jehanzebahsan@gmail.com>
2020-07-24 10:05:37 +0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# siebel-ansible-automation
- Siebel IP17 automation using ansible
+# ansible-siebel
+Ansible roles to install a Siebel Demo Environment
+
+These roles will download, install, configure a complete Siebel Installation.
+A One-Click Siebel-Admin solution.
+
+**Installation**
+
+* git clone https://github.com/henkwiedig/ansible-siebel
+* cd ansible-siebel
+* cp roles/software-repository/tasks/mos-credentials-example.yml roles/software-repository/tasks/mos-credentials.yml
+* Edit roles/software-repository/tasks/mos-credentials.yml
+* Edit inventory/sample
+* export ANSIBLE_INVENTORY=`pwd`/inventory/sample
+* ansible-playbook siebel-master.yml
---
+++ automate-architecture/launch-siebel/ent1.sh
@@ -0,0 +1,41 @@
+ROOT=/home/demoadmin/ConfiguringSiebel/Containerization/Docker/ol7/manage/scripts/automate-architecture
+PV=/var/lib/docker/volumes/PV
+CONTAINERBASE=registry.local.com:5000/siebel
+DOMAIN=company.com
+ENTERPRISE1=ENT1
+DBHOST=oracle12c
+DBPORT=1521
+DBINST=SAMPLE.localdomain
+TNSCONN=SIEBELDB
+DOCKERNET=siebelnet
+TBLO=SIEBEL
+PORT=443
+USER=SADMIN
+PASSWD=Welcome1
+GWUSER=SADMIN
+GWPASSWD=Welcome1
+ANONUSER=GUESTCST
+ANONPASS=Welcome1
+RUNASUSER=9999
+ENTPROFILE=ENT
+HWSERVER=localhost
+COMPGRPS=callcenter,siebelwebtools,eai
+SES1SERVERNAME=SES
+
+readonly compsToManual=(
+	'BusIntBatchMgr'
+	'CustomAppObjMgr_enu'
+	'SServiceObjMgr_enu'
+	'eServiceObjMgr_enu'
+	'JMSReceiver'
+	'InfraEAIOutbound'
+	'SMQReceiver'
+	'MSMQRcvr'
+	'MqSeriesSrvRcvr'
+	'MqSeriesAMIRcvr'
+	'JMSReceiver'
+	'EIM'
+	'CustomAppObjMgr_enu'
+	'BusIntMgr'
+	'BusIntBatchMgr'
+)
